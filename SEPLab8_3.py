2baa90bc2a5c2a442c517843c67faec51ba8963b
UndetectedCheats <61540892+kasivit8@users.noreply.github.com>
2020-03-12 19:40:10 +0700

    Ok

--- SEPLab8_3.py
+++ SEPLab8_3.py
@@ -1,11 +1,11 @@
 import turtle as t
 class Disk:
-    def __init__(self,name,x,y,h,w,colour="blue"):
+    def __init__(self,name,x,y,h,w):
         self.name = name
         self.h = h
         self.w = w
         self.t = t.Turtle()
-        self.color = colour
+        self.color = "blue"
         self.x = x
         self.y = y
         self.t.penup()
@@ -29,7 +29,7 @@ class Disk:
         self.t.end_fill()
         self.t.penup()
 
-     def cleardisk(self):
+    def cleardisk(self):
         self.t.pendown()
         self.t.setheading(0)
         self.t.color("white")
@@ -49,9 +49,55 @@ class Disk:
 
     def newpos(self,x,y):
         self.x = x
-@@ -88,32 +70,17 @@ def showpole(self):
+        self.y = y
+        self.t.penup()
+        self.t.goto(x,y)
+class Pole:
+    def __init__(self,name,x,y):
+        self.name = name
+        self.stack = []
+        self.top = 0
+        self.x = x
+        self.y = y
+        self.w = 20
+        self.h = 100
+        self.thickness = 1
+        self.length = 100
+        self.color = "grey"
+        self.t = t.Turtle()
+        self.t.penup()
+        self.t.goto(x,y)
+    
+    def showpole(self):
+        self.t.pendown()
+        self.t.setheading(0)
+        self.t.color(self.color)
+        self.t.pencolor("black")
+        self.t.begin_fill()
+        self.t.fd(self.w/2)
+        self.t.left(90)
+        self.t.fd(self.h)
+        self.t.left(90)
+        self.t.fd(self.w)
+        self.t.left(90)
+        self.t.fd(self.h)
+        self.t.left(90)
+        self.t.fd(self.w/2)
         self.t.end_fill()
         self.t.penup()
+    
+    def pushdisk(self, disk):
+        self.top += 1
+        self.stack.append(disk)
+        disk.newpos(self.x, (self.y+disk.h)*len(self.stack))
+        disk.showdisk()
+
+    def popdisk(self):
+        disk = self.stack.pop()
+        disk.cleardisk()
+        disk.newpos(self.x, self.length+50)
+        self.top -= 1
+        return disk
 
 class Hanoi(object):
     def __init__(self,n=3,start = "A",workspace = "B", destination = "C"):
@@ -64,11 +110,7 @@ class Hanoi(object):
         self.destinationp.showpole()
         for i in range(n):
             self.startp.pushdisk(Disk("d"+str(i),0,i*150,20,(n-1)*30))
-    def pushdisk(self, disk):
-        self.top += 1
-        self.stack.append(disk)
-        disk.newpos(self.x, (self.y+disk.height)*len(self.stack))
-
+    
     def move_disk(self,start,destination):
         disk = start.popdisk()
         destination.pushdisk(disk)
@@ -82,11 +124,7 @@ class Hanoi(object):
             self.move_tower(n-1,w,d,s)
     def solve(self):
         self.move_tower(3,self.startp,self.destinationp,self.workspacep)
-    def popdisk(self):
-        disk = self.stack.pop()
-        disk.newpos(self.x, self.length+50)
-        self.top -= 1
-        return disk
+
 
 
 h = Hanoi()
