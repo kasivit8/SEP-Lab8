0225058e0c3169fbdf034470a9a4e528ad9dc470
mekhp <61541267+mekhp@users.noreply.github.com>
2020-03-12 19:20:56 +0700

    push pop

--- SEPLab8_3.py
+++ SEPLab8_3.py
@@ -30,24 +30,6 @@ class Disk:
         self.t.fd(self.w/2)
         self.t.end_fill()
         self.t.penup()
-    
-     def cleardisk(self):
-        self.t.pendown()
-        self.t.setheading(0)
-        self.t.color("white")
-        self.t.pencolor("white")
-        self.t.begin_fill()
-        self.t.fd(self.w / 2)
-        self.t.left(90)
-        self.t.fd(self.h)
-        self.t.left(90)
-        self.t.fd(self.w)
-        self.t.left(90)
-        self.t.fd(self.h)
-        self.t.left(90)
-        self.t.fd(self.w / 2)
-        self.t.end_fill()
-        self.t.penup()
 
     def newpos(self,x,y):
         self.x = x
@@ -88,32 +70,17 @@ class Pole:
         self.t.end_fill()
         self.t.penup()
 
-class Hanoi(object):
-    def __init__(self,n=3,start = "A",workspace = "B", destination = "C"):
-
-        self.startp  = Pole(start,0,0)
-        self.workspacep = Pole(workspace,150,0)
-        self.destinationp = Pole(destination,300,0)
-        self.startp.showpole()
-        self.workspacep.showpole()
-        self.destinationp.showpole()
-        for i in range(n):
-            self.startp.pushdisk(Disk("d"+str(i),0,i*150,20,(n-1)*30))
-
-    def move_disk(self,start,destination):
-        disk = start.popdisk()
-        destination.pushdisk(disk)
-
-    def move_tower(self,n,s,d,w):
-        if n == 1:
-            self.move_disk(s,d)
-        else:
-            self.move_tower(n-1,s,w,d)
-            self.move_disk(s,d)
-            self.move_tower(n-1,w,d,s)
-    def solve(self):
-        self.move_tower(3,self.startp,self.destinationp,self.workspacep)
+    def pushdisk(self, disk):
+        self.top += 1
+        self.stack.append(disk)
+        disk.newpos(self.x, (self.y+disk.height)*len(self.stack))
 
 
-h = Hanoi()
-h.solve()
+    def popdisk(self):
+        disk = self.stack.pop()
+        disk.newpos(self.x, self.length+50)
+        self.top -= 1
+        return disk
+
+
+        
