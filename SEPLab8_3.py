4d92997e60e7e00af1028a40fa65cec47432e41e
UndetectedCheats <61540892+kasivit8@users.noreply.github.com>
2020-03-12 19:07:25 +0700

    Part2

--- SEPLab8_3.py
+++ SEPLab8_3.py
@@ -1,12 +1,12 @@
 import turtle as t
 
 class Disk:
-    def __init__(self,name,x,y,h,w,colour):
+    def __init__(self,name,x,y,h,w):
         self.name = name
         self.h = h
         self.w = w
         self.t = t.Turtle()
-        self.color = colour
+        self.color = "blue"
         self.x = x
         self.y = y
         self.t.penup()
@@ -36,4 +36,37 @@ class Disk:
         self.y = y
         self.t.penup()
         self.t.goto(x,y)
+
+
+class Pole:
+    def __init__(self,name,x,y):
+        self.name = name
+        self.stack = []
+        self.top_pos = 0
+        self.x = x
+        self.y = y
+        self.thickness = 1
+        self.length = 100
+        self.color = "grey"
+        self.t = t.Turtle()
+        self.t.penup()
+        self.t.goto(x,y)
+    
+    def showpole(self):
+        self.t.pendown()
+        self.t.setheading(0)
+        self.t.color(self.color)
+        self.t.pencolor("black")
+        self.t.begin_fill()
+        self.t.fd(self.w/2)
+        self.t.left(90)
+        self.t.fd(self.h)
+        self.t.left(90)
+        self.t.fd(self.w)
+        self.t.left(90)
+        self.t.fd(self.h)
+        self.t.left(90)
+        self.t.fd(self.w/2)
+        self.t.end_fill()
+        self.t.penup()
 
