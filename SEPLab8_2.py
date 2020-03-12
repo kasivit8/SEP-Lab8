faa66e9f81ec7f764262c7d7308f2ad5effb6a06
Chanadech <60448393+chanadech@users.noreply.github.com>
2020-03-12 18:42:12 +0700

    hgjhg

--- SEPLab8_2.py
+++ SEPLab8_2.py
@@ -3,11 +3,12 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 
+
 class Animation_area(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
-        self.setMinimumSize(500,500)
-        
+        self.setMinimumSize(500, 500)
+
         self.arena_w = 500
         self.arena_h = 500
 
@@ -24,6 +25,13 @@ class Simple_animation_window(QWidget):
         self.setLayout(layout)
         self.setMinimumSize(530, 600)
 
+        self.b1 = QPushButton("Clear")
+        self.b1.toggle()
+        layout.addWidget(self.b1)
+        self.setWindowTitle("A simple paint program ")
+
+
+
 
 def main():
     app = QApplication(sys.argv)
@@ -31,6 +39,7 @@ def main():
     w.show()
 
     return app.exec_()
+
 
 if __name__ == '__main__':
     sys.exit(main())
